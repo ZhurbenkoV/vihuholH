27ef51bd707b570da55651041eec714f21b872e1
ZhurbenkoV <akel.design@inbox.ru>
2020-09-13 17:28:07 +0400

    init

---
+++ vihuhol.h
@@ -0,0 +1,41 @@
+#ifndef VIHUHUL
+#define VIHUHUL
+
+#define касе case
+#define бреак break
+#define цхар char
+#define класс class
+#define конст const
+#define континуе continue
+#define дефаулт default
+#define ду do
+#define елсе else
+#define фалсе false
+#define труе true
+#define фор for
+#define иф if
+#define нев new
+#define делете delete
+#define намеспасе namespace
+#define нуллптр nullptr
+#define привате private
+#define протектед protected
+#define публик public
+#define ретурн return
+#define статик static
+#define статик_каст static_cast
+#define структ struct
+#define енум enum
+#define свитцх switch
+#define темплате template
+#define усинг using
+#define вхиле while
+#define инт int
+#define доубле double
+#define флоат float
+#define лонг long
+#define унсигнед unsigned
+#define схорт short
+#define инклуде include
+
+#endif
